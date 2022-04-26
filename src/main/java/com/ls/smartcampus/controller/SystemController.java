7ff5d818bcca4816e99604eebe5aca8ccd16ff89
ls <1537515284@qq.com>
2022-04-26 21:26:36 +0800

    新增 /login 登录接口

--- src/main/java/com/ls/smartcampus/controller/SystemController.java
+++ src/main/java/com/ls/smartcampus/controller/SystemController.java
@@ -1,9 +1,19 @@
 package com.ls.smartcampus.controller;
 
+import com.ls.smartcampus.pojo.Admin;
+import com.ls.smartcampus.pojo.LoginForm;
+import com.ls.smartcampus.pojo.Student;
+import com.ls.smartcampus.pojo.Teacher;
+import com.ls.smartcampus.service.AdminService;
+import com.ls.smartcampus.service.StudentService;
+import com.ls.smartcampus.service.TeacherService;
 import com.ls.smartcampus.util.CreateVerificationCodeImage;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.ls.smartcampus.util.JwtHelper;
+import com.ls.smartcampus.util.Result;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import javax.imageio.ImageIO;
 import javax.servlet.ServletOutputStream;
@@ -12,10 +22,88 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/sms/system")
+@RequiredArgsConstructor
 public class SystemController {
+
+    @NonNull
+    private final AdminService adminService;
+    @NonNull
+    private final StudentService studentService;
+    @NonNull
+    private final TeacherService teacherService;
+
+
+    @PostMapping("/login")
+    public Result login(@RequestBody LoginForm loginForm, HttpSession session) {
+        // 验证码校验
+        String originalVerificationCode = (String) session.getAttribute("verificationCode");
+        String receivedVerificationCode = loginForm.getVerifiCode();
+        if ("".equals(originalVerificationCode) || null == originalVerificationCode) {
+            return Result.fail().message("验证码失效，请刷新后重试");
+        }
+        if (!originalVerificationCode.equalsIgnoreCase(receivedVerificationCode)) {
+            return Result.fail().message("验证码有误,请重新输入");
+        }
+        // 从session域中移除现有验证码
+        session.removeAttribute("verificationCode");
+        // 分用户类型进行校验
+
+        // 准备一个map 用于存放响应的数据
+        Map<String, Object> map = new LinkedHashMap<>();
+        switch (loginForm.getUserType()) {
+            case 1:
+                try {
+                    Admin admin = adminService.login(loginForm);
+                    if (null != admin) {
+                        // 用户的类型和用户id转换成一个密文，以"token"的名称向客户端反馈
+                        String token = JwtHelper.createToken(admin.getId().longValue(), loginForm.getUserType());
+                        map.put("token", token);
+                    } else {
+                        throw new RuntimeException("用户名或者密码有误");
+                    }
+                    return Result.ok(map);
+                } catch (RuntimeException e) {
+                    e.printStackTrace();
+                    return Result.fail().message(e.getMessage());
+                }
+            case 2:
+                try {
+                    Student student = studentService.login(loginForm);
+                    if (null != student) {
+                        // 用户的类型和用户id转换成一个密文，以"token"的名称向客户端反馈
+                        String token = JwtHelper.createToken(student.getId().longValue(), loginForm.getUserType());
+                        map.put("token", token);
+                    } else {
+                        throw new RuntimeException("用户名或者密码有误");
+                    }
+                    return Result.ok(map);
+                } catch (RuntimeException e) {
+                    e.printStackTrace();
+                    return Result.fail().message(e.getMessage());
+                }
+            case 3:
+                try {
+                    Teacher teacher = teacherService.login(loginForm);
+                    if (null != teacher) {
+                        // 用户的类型和用户id转换成一个密文，以"token"的名称向客户端反馈
+                        String token = JwtHelper.createToken(teacher.getId().longValue(), loginForm.getUserType());
+                        map.put("token", token);
+                    } else {
+                        throw new RuntimeException("用户名或者密码有误");
+                    }
+                    return Result.ok(map);
+                } catch (RuntimeException e) {
+                    e.printStackTrace();
+                    return Result.fail().message(e.getMessage());
+                }
+        }
+        return Result.fail().message("查无此用户");
+    }
 
 
     @GetMapping("/getVerifiCodeImage")
